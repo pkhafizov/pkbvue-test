5a8250ba92606ef38610c2008cca9cce4941810e
pkhafizov <pkhafizov@gmail.com>
2020-02-02 14:44:49 +0700

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row v-if="item.heading" :key="item.heading" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item v-for="(child, i) in item.children" :key="i" link>
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>{{ child.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else :key="item.text" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">PKBVUE-TEST</span>
+      </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center"></v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: 'mdi-contacts', text: 'Клиенты' },
+      {
+        icon: 'mdi-chevron-up',
+        'icon-alt': 'mdi-chevron-down',
+        text: 'Портфели',
+        model: false,
+        children: [
+          { text: 'Все портфели' },
+          { text: 'Ежемесячный' },
+          { text: 'Эффективность' },
+          { text: 'Эффективность по месяцам' }
+        ],
+      },
+      { icon: 'mdi-layers-triple-outline', text: 'Кредит' },
+      { icon: 'mdi-layers-triple', text: 'Дебет' },
+      { icon: 'mdi-calendar-clock', text: 'Календарь' }
+    ],
+  }),
+}
+</script>
